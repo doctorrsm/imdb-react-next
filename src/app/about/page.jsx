fa1330147530ced1186eee42c039799bac3050fa
dr.bibelman <dr.bibelman@gmail.com>
2023-04-06 19:29:05 +0300

    create about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,50 @@
+const About = () => {
+  return (
+    <div className="max-w-6xl mx-auto space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        Welcome to IBDM Clone, the website that aims to provide a comprehensive
+        repository of business data models for organizations of all sizes. Our
+        mission is to empower business analysts, data architects, and IT
+        professionals with the tools they need to design, document, and
+        communicate data models that support the strategic objectives of their
+        organizations.
+      </p>
+
+      <p>
+        IBDM Clone is a community-driven project that brings together experts
+        from various industries to share their knowledge and experience in the
+        field of data modeling. We believe that by collaborating and sharing
+        best practices, we can create a more unified and efficient approach to
+        data management that benefits everyone.
+      </p>
+
+      <p>
+        Our goal is to provide a platform where business users, IT
+        professionals, and software vendors can come together to explore new
+        ideas, solve complex problems, and discover innovative solutions to the
+        challenges of data modeling. With our growing database of data models,
+        tutorials, and resources, IBDM Clone is the go-to source for anyone
+        interested in data modeling.
+      </p>
+
+      <p>
+        At IBDM Clone, we are committed to promoting excellence in data modeling
+        and advocating for best practices in data management. We believe that
+        data is the lifeblood of any organization, and that by taking a
+        strategic approach to data modeling, organizations can unlock new
+        insights, improve decision-making, and achieve better business outcomes.
+      </p>
+
+      <p>
+        Join the IBDM Clone community today and start exploring the world of
+        data modeling. Whether you're an experienced data professional or just
+        starting out, we have something for everyone. From beginner-friendly
+        tutorials to advanced data modeling techniques, IBDM Clone is the
+        ultimate resource for all your data modeling needs.
+      </p>
+    </div>
+  );
+};
+
+export default About;
